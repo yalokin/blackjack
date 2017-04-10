e36348cbbb84f397d98007255d82228eb6c60228
Nikolay Ch <chumin.n@gmail.com>
2017-04-10 19:23:49 +0300

    Initial

---
+++ deck.rb
@@ -0,0 +1,21 @@
+class Deck
+
+  SUITS = ['+', '<3', '^', '<>'].freeze
+  CARDS = {
+    '2' => 2, '3'  => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
+    '9' => 9, '10' => 10, 'K' => 10, 'J' => 10, 'Q' => 10, 'A' => 1
+  }
+
+  def initialize
+    make_deck
+  end
+
+  def make_deck
+    @deck = []
+    SUITS.each do |suit|
+      CARDS.each do |key, value|
+      @deck << [key + suit, value]
+      end
+    end
+  end
+end
